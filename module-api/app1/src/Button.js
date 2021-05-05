0abe072bcab3e5fba5bdef7f073fa8a29e5f67bf
Zack Jackson <zackary.l.jackson@gmail.com>
2021-05-05 09:58:01 -0700

    Module API

--- module-api/app1/src/Button.js
+++ module-api/app1/src/Button.js
@@ -1,6 +1,6 @@
 import React from "react";
-import PropTypes from 'prop-types'
-import Joi from 'joi'
+import PropTypes from "prop-types";
+import Joi from "joi";
 
 const style = {
   background: "#800",
@@ -8,23 +8,24 @@ const style = {
   padding: 12,
 };
 
-export const Button = (props) => <button style={style}>{props.children}</button>;
-const FederatedButton = (props) => {
+export const Button = (props) => (
+  <button style={style}>{props.children}</button>
+);
 
+const FederatedButton = (props) => {
   // specify prop schema
   const schema = Joi.object({
-    children: [Joi.string(),Joi.array(),Joi.object()]
-  })
+    children: [Joi.string(), Joi.array(), Joi.object()],
+  });
   // memoize validation result
-  const {error} = React.useMemo(()=>schema.validate(props),[])
-  if(error) {
+  const { error } = React.useMemo(() => schema.validate(props), []);
+  if (error) {
     // if it fails, return a fallback, or allow a fallback to be passed in
-    return <span>Validation Error</span>
+    return <span>Validation Error</span>;
   }
   // if validation passes, expose the underlaying component.
   // this wrapper also allows <button> to change without the consumer having to re-implement it.
   // the wrapper can serve as an adapter
-  return <Button>{props.children}</Button>
-
-}
+  return <Button>{props.children}</Button>;
+};
 export default FederatedButton;
